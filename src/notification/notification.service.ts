88545ab0704df74a95d9cbec40f02f610ed4bb81
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-02-12 02:26:26 +0900

    Modify: Modify notification, opus domain

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -6,65 +6,120 @@ export class NotificationService {
   constructor(private prisma: PrismaService) {}
 
   async createNotification(userId: number, body) {
-    const { title, content, grade, className } = body;
-
-    const students = await this.prisma.users.findMany({
-      where: {
-        grade,
-        className,
-      },
-      select: {
-        userId: true,
-      },
-    });
-
-    const notificationData = students.map((student) => ({
+    const {
       title,
       content,
       grade,
       className,
-      userId: student.userId,
-    }));
+      koreanClassName,
+      englishClassName,
+    } = body;
 
-    const result = await this.prisma.notifications.createMany({
-      data: notificationData,
-    });
+    let students;
 
-    return result;
+    if (koreanClassName) {
+      students = await this.prisma.users.findMany({
+        where: {
+          grade,
+          koreanClassName,
+        },
+        select: {
+          userId: true,
+        },
+      });
+
+      const notificationData = students.map((student) => ({
+        title,
+        content,
+        grade,
+        koreanClassName,
+        userId: student.userId,
+      }));
+
+      const result = await this.prisma.notifications.createMany({
+        data: notificationData,
+      });
+      return result;
+    } else {
+      students = await this.prisma.users.findMany({
+        where: {
+          grade,
+          englishClassName,
+        },
+        select: {
+          userId: true,
+        },
+      });
+
+      const notificationData = students.map((student) => ({
+        title,
+        content,
+        grade,
+        englishClassName,
+        userId: student.userId,
+      }));
+
+      const result = await this.prisma.notifications.createMany({
+        data: notificationData,
+      });
+      return result;
+    }
   }
 
   async fetchNotifications(user) {
-    let queryGrade;
-    let queryClassName;
+    let notifications = [];
 
     if (user.role === "parent" && user.studentId !== 0) {
       const student = await this.prisma.users.findUnique({
         where: { userId: user.studentId },
         select: {
           grade: true,
-          className: true,
+          koreanClassName: true,
+          englishClassName: true,
         },
       });
 
       if (student) {
-        queryGrade = student.grade;
-        queryClassName = student.className;
-      } else {
-        return [];
+        // 한국어 클래스 알림 조회
+        const koreanClassNotifications =
+          await this.prisma.notifications.findMany({
+            where: {
+              grade: student.grade,
+              AND: [{ className: student.koreanClassName }],
+            },
+          });
+
+        const englishClassNotifications =
+          await this.prisma.notifications.findMany({
+            where: {
+              grade: student.grade,
+              AND: [{ className: student.englishClassName }],
+            },
+          });
+
+        notifications = [
+          ...koreanClassNotifications,
+          ...englishClassNotifications,
+        ];
       }
     } else {
-      queryGrade = user.grade;
-      queryClassName = user.className;
+      notifications = await this.prisma.notifications.findMany({
+        where: {
+          grade: user.grade,
+          OR: [
+            { className: user.koreanClassName },
+            { className: user.englishClassName },
+          ],
+        },
+      });
     }
 
-    const result = await this.prisma.notifications.findMany({
-      where: {
-        grade: queryGrade,
-        className: queryClassName,
-      },
-    });
+    const uniqueNotifications = notifications.filter(
+      (v, i, a) =>
+        a.findIndex((t) => t.notificationId === v.notificationId) === i
+    );
 
-    return result;
+    return uniqueNotifications;
   }
 
   async updateIsRead(id: number) {
